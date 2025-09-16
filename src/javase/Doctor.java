c292bd37fba5aedacd0891c33114b5c3d2d785cd
HarolTorrado73 <ubuntohcmelot@gmail.com>
2025-09-15 20:27:57 -0500

    Mi primer  commit

---
+++ src/javase/Doctor.java
@@ -0,0 +1,13 @@
+package javase;
+public class Doctor {
+    int id;
+    String nombre;
+    String espeliacion;
+    
+    public void MostrarDoctor(){
+        System.out.println("Id: "+id);
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Especialidad: "+espeliacion);
+    }
+
+}
---
+++ src/javase/JavaS.java
@@ -0,0 +1,14 @@
+package javase;
+
+public class JavaS {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Doctor doctor1 = new Doctor();
+        doctor1.id = 1;
+        doctor1.nombre = "Pedro";
+        doctor1.espeliacion = "Cardiologia";
+        doctor1.MostrarDoctor();
+    }
+    
+}
